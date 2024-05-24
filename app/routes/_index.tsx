f4150e5f442a95765b3a40b1db0e16eff5aba175
Kannavi Mishra <79000269+cloudcreatr@users.noreply.github.com>
2024-05-24 12:16:42 +0000

    chore: Refactor index page to use lazy loading for form data and form submission

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -7,25 +7,46 @@ import { FormEntry } from "schema/form";
 import { Button } from "~/component/ui/button";
 import { Input } from "~/component/ui/input";
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    {
-      name: "description",
-      content: "Welcome to Remix! Using Vite and Cloudflare!",
-    },
-  ];
-};
+
 
 export default function Index() {
- 
-
+  const db = useLoaderData<typeof loader>()
+  const f = useFetcher()
   return (
     <div className="flex flex-col justify-center items-center h-screen">
 
 
-      <Data />
-      <Form />
+      <div className="flex flex-row justify-center items-center" >
+        <p className=" bg-gray-50 border-2 border-gray-100 rounded-lg shadow-2xl p-6 m-4 font-sans font-bold ">
+          <h1>Form Data</h1>
+
+          <Suspense fallback={<div className="bg-red-500">Loading</div>}>
+            <Await resolve={db.dblist}>
+              {(data) => {
+                return <div>
+                  {data.map((item, index) => {
+                    return <div key={index} className="border-2 border-gray-200 p-2 m-2 rounded-lg">
+                      <h1>{item.name}</h1>
+                      <p>{item.message}</p>
+                    </div>
+                  })}
+                </div>
+
+              }}
+            </Await>
+          </Suspense>
+        </p>
+      </div>
+      <div className="flex flex-row justify-center items-center" >
+        <p className=" bg-gray-50 border-2 border-gray-100 rounded-lg shadow-2xl p-6 m-4 font-sans font-bold ">
+          <f.Form className="space-y-4" method="post">
+            <Input placeholder="name" type="text" name="name" required />
+            <Input type="text" name="message" required placeholder="message here" />
+            <Button className="w-full " type="submit">Submit</Button>
+
+          </f.Form>
+        </p>
+      </div>
     </div>
   );
 }
@@ -44,33 +65,6 @@ export async function loader({
   })
 }
 
-function Data() {
-  const db = useLoaderData<typeof loader>()
-  return (
-    <div className="flex flex-row justify-center items-center" >
-      <p className=" bg-gray-50 border-2 border-gray-100 rounded-lg shadow-2xl p-6 m-4 font-sans font-bold ">
-        <h1>Form Data</h1>
-
-        <Suspense fallback={<div>Loading</div>}>
-          <Await resolve={db.dblist}>
-            {(data) => {
-              return <div>
-                {data.map((item, index) => {
-                  return <div key={index} className="border-2 border-gray-200 p-2 m-2 rounded-lg">
-                    <h1>{item.name}</h1>
-                    <p>{item.message}</p>
-                  </div>
-                })}
-              </div>
-
-            }}
-          </Await>
-        </Suspense>
-      </p>
-    </div>
-
-  )
-}
 
 
 
@@ -78,21 +72,6 @@ function Data() {
 
 
 
-function Form() {
-  const f = useFetcher()
-  return (
-    <div className="flex flex-row justify-center items-center" >
-      <p className=" bg-gray-50 border-2 border-gray-100 rounded-lg shadow-2xl p-6 m-4 font-sans font-bold ">
-        <f.Form className="space-y-4" method="post">
-          <Input placeholder="name" type="text" name="name" required />
-          <Input type="text" name="message" required placeholder="message here" />
-          <Button className="w-full " type="submit">Submit</Button>
-
-        </f.Form>
-      </p>
-    </div>
-  )
-}
 
 export async function action({ request, context }: ActionFunctionArgs) {
   const db = drizzle(context.cloudflare.env.DB)
