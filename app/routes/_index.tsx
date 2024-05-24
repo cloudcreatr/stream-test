d3cd2137d4ea7c78ea7021c66aed02cce2d54245
Kannavi Mishra <79000269+cloudcreatr@users.noreply.github.com>
2024-05-24 12:35:22 +0000

    chore: Refactor index page to use lazy loading for form data and form submission

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -8,49 +8,6 @@ import { Button } from "~/component/ui/button";
 import { Input } from "~/component/ui/input";
 
 
-
-export default function Index() {
-  const db = useLoaderData<typeof loader>()
-  const f = useFetcher()
-  return (
-    <div className="flex flex-col justify-center items-center h-screen">
-
-
-      <div className="flex flex-row justify-center items-center" >
-        <p className=" bg-gray-50 border-2 border-gray-100 rounded-lg shadow-2xl p-6 m-4 font-sans font-bold ">
-          <h1>Form Data</h1>
-
-          <Suspense fallback={<div className="bg-red-500">Loading</div>}>
-            <Await resolve={db.dblist}>
-              {(data) => {
-                return <div>
-                  {data.map((item, index) => {
-                    return <div key={index} className="border-2 border-gray-200 p-2 m-2 rounded-lg">
-                      <h1>{item.name}</h1>
-                      <p>{item.message}</p>
-                    </div>
-                  })}
-                </div>
-
-              }}
-            </Await>
-          </Suspense>
-        </p>
-      </div>
-      <div className="flex flex-row justify-center items-center" >
-        <p className=" bg-gray-50 border-2 border-gray-100 rounded-lg shadow-2xl p-6 m-4 font-sans font-bold ">
-          <f.Form className="space-y-4" method="post">
-            <Input placeholder="name" type="text" name="name" required />
-            <Input type="text" name="message" required placeholder="message here" />
-            <Button className="w-full " type="submit">Submit</Button>
-
-          </f.Form>
-        </p>
-      </div>
-    </div>
-  );
-}
-
 export async function loader({
   request,
   context
@@ -64,6 +21,42 @@ export async function loader({
     dblist: data
   })
 }
+
+export default function Index() {
+  const {
+    dblist
+  } = useLoaderData<typeof loader>()
+  const f = useFetcher()
+  return (
+    <div className="flex flex-col space-y-5 justify-center items-center h-screen">
+          <Suspense fallback={<div className="bg-red-500">Loading</div>}>
+            <Await resolve={dblist}>
+              {(data) => {
+                return <div>
+                  {data.map((item, index) => {
+                    return <div key={index} className="border-2 border-gray-200 p-2 m-2 rounded-lg">
+                      <h1>{item.name}</h1>
+                      <p>{item.message}</p>
+                    </div>
+                  })}
+                </div>
+
+              }}
+            </Await>
+          </Suspense>
+      
+          <f.Form className="space-y-4" method="post">
+            <Input placeholder="name" type="text" name="name" required />
+            <Input type="text" name="message" required placeholder="message here" />
+            <Button className="w-full " type="submit">Submit</Button>
+
+          </f.Form>
+      
+      
+    </div>
+  );
+}
+
 
 
 
